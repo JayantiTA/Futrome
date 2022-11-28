b3307d758eaa1a147981c28962014a9005e3b873
JayantiTA <jayjayanti18@gmail.com>
2022-11-28 23:03:56 +0700

    feat: get reservation by id and user id

---
+++ pages/api/reservations/[reservationId].js
@@ -0,0 +1,54 @@
+import nextConnect from 'next-connect';
+
+import auth from '../../../middleware/auth';
+import connectToDatabase from '../../../lib/mongoose';
+import Reservation from '../../../models/reservation';
+import Payment from '../../../models/payment';
+
+import { encodeBase64Image } from '../../../helper/image';
+import { notFoundError, errorHandler } from '../../../helper/error';
+
+const handler = nextConnect({
+  onError: errorHandler,
+  onNoMatch: (req, res) => {
+    res.status(405).json({
+      message: 'Method not allowed',
+      success: false,
+    });
+  },
+});
+
+handler
+  .use(auth())
+  .get(async (req, res, next) => {
+    await connectToDatabase();
+    const filter = { _id: req.query.reservationId };
+    if (req.user.role !== 'admin') {
+      filter.buyer_id = req.user._id;
+    }
+    const reservation = await Reservation.findOne(filter);
+
+    if (!reservation) {
+      return next({
+        name: notFoundError,
+        message: 'Reservation not found',
+      });
+    }
+
+    let payment;
+    if (reservation.status === 'paid') {
+      payment = await Payment.findOne({ reservation_id: reservation._id });
+      payment.attachment = encodeBase64Image(payment.attachment);
+    }
+
+    return res.json({
+      data: {
+        reservation,
+        payment,
+      },
+      message: 'Success',
+      success: true,
+    });
+  });
+
+export default handler;
