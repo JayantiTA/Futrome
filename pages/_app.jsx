3bb0046f451d544e21a846809ba0caa4f46d9ff5
JayantiTA <jayjayanti18@gmail.com>
2022-11-30 15:40:14 +0700

    feat: use createTheme from material ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,48 @@
+import * as React from 'react';
 import '../styles/globals.css';
+import Head from 'next/head';
+import NoSsr from '@mui/material/NoSsr';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+import createEmotionCache from '../helper/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
+const theme = createTheme({
+  palette: {
+    grey: {
+      main: '#333333',
+      light: '#E5EBE3',
+    },
+    green: {
+      main: '#195A00',
+      hover: '#707957',
+      light: '#B6BCA4',
+    },
+    white: {
+      main: '#FFFFFF',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto',
+  },
+});
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    // eslint-disable-next-line react/react-in-jsx-scope
-    <Component {...pageProps} />
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <NoSsr>
+          <Component {...pageProps} />
+        </NoSsr>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
-
-export default MyApp;
