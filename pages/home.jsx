400b34a2c04ce6e9f3cea1b5a8d0506cf8251b72
JayantiTA <jayjayanti18@gmail.com>
2022-12-01 20:46:23 +0700

    feat: create home page

---
+++ pages/home.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Image from 'next/image';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+
+import Navbar from '../components/Navbar';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    backgroundColor: theme.palette.green.main,
+    color: theme.palette.white.main,
+    padding: '0.5rem 1rem',
+    textTransform: 'none',
+    fontSize: 17,
+    marginTop: 30,
+    '&:hover': {
+      backgroundColor: theme.palette.green.hover,
+    },
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Navbar />
+      <Box sx={{
+        display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center', my: 10,
+      }}
+      >
+        <Card sx={{ boxShadow: 'none', maxWidth: 600, margin: 5 }}>
+          <Typography variant="h6" sx={{ fontFamily: 'Miniver', color: '#195A00' }}>Futrome</Typography>
+          <Typography fontWeight={700} variant="h3">
+            Persiapkan rumah masa
+            depan dari sekarang
+          </Typography>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            Konsep baru sebuah taman pemakaman Islam unik dan terpadu
+            kini hadir di Indonesia.
+          </Typography>
+          <Button className={classes.button}>
+            Pesan Sekarang
+          </Button>
+        </Card>
+        <Image src="/home/image.svg" width={450} height={450} />
+      </Box>
+    </>
+  );
+}
